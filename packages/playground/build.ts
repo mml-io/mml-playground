45e03cc9a7c90b520e7aaed4cadf5a665ae4cc3b
Daniel J. Rollins <me@deej.io>
2024-07-31 22:46:12 +0100

    add comments MML plugin sections

--- packages/playground/build.ts
+++ packages/playground/build.ts
@@ -20,12 +20,19 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
 
   const results: esbuild.BuildResult[] = [];
 
+  // We create a new non-root instance of the plugin anytime we need to run a child build process
+  // This signifies to the the child plugin that it should store its result in the `results` array
+  // so that the root instance can merge them for the final result.
   const makePlugin = (isRoot: boolean = false): esbuild.Plugin => ({
     name: "mml",
     setup(build) {
+      // We rely on the metfile to perform JS-to-HTML embedding and file renames.
       build.initialOptions.metafile = true;
       const outdir = resolve(__dirname, build.initialOptions.outdir ?? "build");
 
+      // Main entry point for any imports that are prefixed with "mml:".
+      // We strip the prefix and resolve the path with esbuild before handing off to
+      // an mml loader.
       build.onResolve({ filter: /^mml:/ }, async (args) => {
         log("onResolve", args);
         const { path, ...rest } = args;
@@ -33,6 +40,9 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
         return { ...result, namespace: "mml" };
       });
 
+      // Loader for any (originally) mml-prefixed paths. This requests the file be built by a
+      // child esbuild instance, however we control the rewriting of the import paths using the
+      // "text" loader to embed the path to the document as a string within the importer.
       build.onLoad({ filter: /.*/, namespace: "mml" }, async (args) => {
         log("onLoad", args);
         const { path } = args;
@@ -47,6 +57,9 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
         return { contents, loader: "text" };
       });
 
+      // Any raw HTML files should just be copied to the build directory
+      // TODO: These could contain script tags with references to local files,
+      //       we may want to consider loading and embedding them directly into the HTML.
       build.onLoad({ filter: /\.html$/ }, async (args) => {
         log("onLoad", args);
         const { path } = args;
@@ -56,11 +69,12 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
 
       build.onEnd(async result => {
         if (!isRoot) {
-          log("onEnd", "pushing results to parent");
           results.push(result);
           return;
         }
 
+        // We are in the root plugin instance. All child instances have finished and
+        // push their results into the array. Now we combine all the results into one.
         const combinedResults = results.reduce((acc, val) => ({
           errors: acc.errors.concat(val.errors),
           warnings: acc.warnings.concat(val.warnings),
@@ -74,11 +88,10 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
 
         Object.assign(result, combinedResults);
 
-        if (!result.metafile) {
-          return;
-        }
+        // If we have a any js files, that do not have a corresponding HTML file,
+        // we need to create one and embed the JavaScript into a <script> tag.
         const jsExt = /\.js$/;
-        const outputs = result.metafile.outputs;
+        const outputs = result.metafile!.outputs;
         for (const [output, meta] of Object.entries(outputs)) {
           if (jsExt.test(output)) {
             const js = await readFile(output, { encoding: "utf8" });
@@ -91,6 +104,8 @@ function mmlPlugin({ verbose = false, removeJS = true }: MMLPluginOptions = {}):
           }
         }
 
+        // For an MML project, we only need the HTML files so we can clean up
+        // any remaining JS files from the build directory and the result outputs.
         if (removeJS) {
           for (const output of Object.keys(outputs)) {
             if (jsExt.test(output)) {
